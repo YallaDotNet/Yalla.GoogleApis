f36e4335ee967937b3f74741d497f527a41227e9
Dmitry Shechtman <damnian@gmail.com>
2015-11-30 01:15:22 +0200

    Add Yalla.GoogleApis

---
+++ src/Yalla.GoogleApis/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Resources;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Yalla.GoogleApis")]
+[assembly: AssemblyDescription("YALLA.NET Google APIs Client logger")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dmitry Shechtman")]
+[assembly: AssemblyProduct("YALLA.NET")]
+[assembly: AssemblyCopyright("Copyright © Dmitry Shechtman 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.*")]
---
+++ src/Yalla.GoogleApis/YallaLogger.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Yalla.GoogleApis
+{
+    /// <summary>
+    /// A logger implementation which makes use of the YALLA.NET library.
+    /// </summary>
+    public class YallaLogger : Google.Apis.Logging.ILogger
+    {
+        private static readonly ILoggerCache<Type, YallaLogger> _cache =
+            new LoggerCache<Type, YallaLogger>();
+
+        private readonly ILog _log;
+
+        private YallaLogger(ILog log)
+        {
+            this._log = log;
+        }
+
+        /// <summary>
+        /// Returns a logger which will be associated with the specified type.
+        /// </summary>
+        /// <param name="type">Type to which this logger belongs.</param>
+        /// <returns>A type-associated logger.</returns>
+        public Google.Apis.Logging.ILogger ForType(Type type)
+        {
+            return _cache.GetOrAdd(type, CreateLogger);
+        }
+
+        /// <summary>
+        /// Returns a logger which will be associated with the specified type.
+        /// </summary>
+        /// <typeparam name="T">Type to which this logger belongs.</typeparam>
+        /// <returns>A type-associated logger.</returns>
+        public Google.Apis.Logging.ILogger ForType<T>()
+        {
+            return ForType(typeof(T));
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether debug output is enabled.
+        /// </summary>
+        /// <value><c>true</c> if debug output is enabled.</value>
+        public bool IsDebugEnabled
+        {
+            get { return _log.IsDebugEnabled; }
+        }
+
+        /// <summary>
+        /// Logs a debug message.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="args"><see cref="String.Format(String,Object[])"/> arguments (if applicable).</param>
+        public void Debug(string message, params object[] args)
+        {
+            _log.DebugFormat(message, args);
+        }
+
+        /// <summary>
+        /// Logs an info message.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="args"><see cref="String.Format(String,Object[])"/> arguments (if applicable).</param>
+        public void Info(string message, params object[] args)
+        {
+            _log.InfoFormat(message, args);
+        }
+
+        /// <summary>
+        /// Logs a warning.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="args"><see cref="String.Format(String,Object[])"/> arguments (if applicable).</param>
+        public void Warning(string message, params object[] args)
+        {
+            _log.WarnFormat(message, args);
+        }
+
+        /// <summary>
+        /// Logs an error message.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="args"><see cref="String.Format(String,Object[])"/> arguments (if applicable).</param>
+        public void Error(string message, params object[] args)
+        {
+            _log.ErrorFormat(message, args);
+        }
+
+        /// <summary>
+        /// Logs an error message resulting from an exception.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        /// <param name="message">The message to log.</param>
+        /// <param name="args"><see cref="String.Format(String,Object[])"/> arguments (if applicable).</param>
+        public void Error(Exception exception, string message, params object[] args)
+        {
+            _log.ErrorFormat(message, exception, args);
+        }
+
+        private static YallaLogger CreateLogger(Type type)
+        {
+            var log = LogManager.GetLogger(type);
+            return new YallaLogger(log);
+        }
+    }
+}
