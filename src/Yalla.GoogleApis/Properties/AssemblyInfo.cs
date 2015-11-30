97ae2d9650c315c6468ad872e26196b7fb10244d
Dmitry Shechtman <damnian@gmail.com>
2015-12-01 01:33:27 +0200

    Move Yalla.snk to ..\..\Properties Extract YallaAssemblyInfo

--- src/Yalla.GoogleApis/Properties/AssemblyInfo.cs
+++ src/Yalla.GoogleApis/Properties/AssemblyInfo.cs
@@ -4,13 +4,6 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Yalla.GoogleApis")]
+[assembly: AssemblyTitle("YALLA.NET Google APIs")]
 [assembly: AssemblyDescription("YALLA.NET Google APIs Client logger")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dmitry Shechtman")]
-[assembly: AssemblyProduct("YALLA.NET")]
-[assembly: AssemblyCopyright("Copyright © Dmitry Shechtman 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 [assembly: AssemblyVersion("1.0.*")]
